f89b83823e8531c8f8c239624f34f583b1d1ab14
João Manoel <joaomanoeldev@gmail.com>
2026-06-21 18:12:35 -0300

    feat: create equals method to Entity

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityID } from "./unique-entity-id";
 
-export class Entity<Props> {
+export abstract class Entity<Props> {
   private _id: UniqueEntityID;
   protected props: Props;
 
@@ -8,8 +8,20 @@ export class Entity<Props> {
     return this._id;
   }
 
-  constructor(props: Props, id?: UniqueEntityID) {
+  protected constructor(props: Props, id?: UniqueEntityID) {
     this._id = id ?? new UniqueEntityID();
     this.props = props;
+  }
+
+  public equals(entity: Entity<unknown>) {
+    if (entity === this) {
+      return true
+    }
+
+    if (entity.id === this._id) {
+      return true
+    }
+
+    return false
   }
 }
