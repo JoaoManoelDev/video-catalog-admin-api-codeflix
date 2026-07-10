d419eae6e9b0c7e446975f486572694e8c6b2875
João Manoel <joaomanoeldev@gmail.com>
2026-07-10 19:33:54 -0300

    feat: create CreateCategoryUseCase and tests

---
+++ src/modules/category/domain/use-cases/create-category-use-case.spec.ts
@@ -0,0 +1,78 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { InMemoryCategoryRepository } from "../../test/repositories/in-memory-category-repository";
+import { CreateCategoryUseCase } from "./create-category-use-case";
+import { Category } from "../entities/category-entity";
+
+let inMemoryCategoryRepository: InMemoryCategoryRepository;
+
+let sut: CreateCategoryUseCase;
+
+describe("Create Category Use Case", () => {
+  beforeEach(() => {
+    inMemoryCategoryRepository = new InMemoryCategoryRepository();
+
+    sut = new CreateCategoryUseCase(inMemoryCategoryRepository);
+  });
+
+  it("should be able to create a new category", async () => {
+    const result = await sut.execute({
+      name: "Category 1",
+      description: "Category 1 description",
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      category: inMemoryCategoryRepository.categories[0],
+    });
+  });
+
+  it("should not be able to create a category with invalid name", async () => {
+    const result = await sut.execute({
+      name: "ab",
+      description: "Category 1 description",
+    });
+
+    expect(result.isLeft()).toBe(true);
+
+    if (result.isLeft()) {
+      expect(result.value.toJSON()).toEqual(
+        expect.arrayContaining([
+          {
+            name: ["name must be longer than or equal to 3 characters"],
+          },
+        ]),
+      );
+    }
+
+    expect(inMemoryCategoryRepository.categories).toHaveLength(0);
+  });
+
+  it("should not be able to create a category with an existing name", async () => {
+    await inMemoryCategoryRepository.create(
+      Category.create({
+        name: "Category 1",
+        description: "Category 1 description",
+      }),
+    );
+
+    const result = await sut.execute({
+      name: "Category 1",
+      description: "Category 1 description",
+    });
+
+    expect(result.isLeft()).toBe(true);
+
+    if (result.isLeft()) {
+      expect(result.value.toJSON()).toEqual(
+        expect.arrayContaining([
+          {
+            name: ["name must be unique"],
+          },
+        ]),
+      );
+    }
+
+    expect(inMemoryCategoryRepository.categories).toHaveLength(1);
+  });
+});
---
+++ src/modules/category/domain/use-cases/create-category-use-case.ts
@@ -0,0 +1,46 @@
+import { Either, left, right } from "@/shared/errors/either";
+import { ValidationErrors } from "@/shared/validators/validation-errors";
+import { Category } from "../entities/category-entity";
+import { CategoryRepository } from "../repositories/category-repository";
+
+interface ICreateCategoryRequest {
+  name: string;
+  description: string;
+}
+
+type ICreateCategoryResponse = Either<
+  ValidationErrors,
+  {
+    category: Category;
+  }
+>;
+
+export class CreateCategoryUseCase {
+  constructor(private categoryRepository: CategoryRepository) {}
+
+  async execute({
+    name,
+    description,
+  }: ICreateCategoryRequest): Promise<ICreateCategoryResponse> {
+    const categoryByName = await this.categoryRepository.findByName(name);
+
+    if (categoryByName) {
+      const validationErrors = new ValidationErrors();
+      validationErrors.addError("name must be unique", "name");
+      return left(validationErrors);
+    }
+
+    const category = Category.create({
+      name,
+      description,
+    });
+
+    if (category.validationErrors.hasErrors()) {
+      return left(category.validationErrors);
+    }
+
+    await this.categoryRepository.create(category);
+
+    return right({ category });
+  }
+}
