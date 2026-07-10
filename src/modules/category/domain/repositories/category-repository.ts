a035ffe7e26a29f7a95acef4ef947b7bb0ecfd79
João Manoel <joaomanoeldev@gmail.com>
2026-07-10 19:32:53 -0300

    feat: create CategoryRepository class

---
+++ src/modules/category/domain/repositories/category-repository.ts
@@ -0,0 +1,9 @@
+import { IPaginatedResult } from "@/shared/repositories/paginated-result";
+import { IQueryParams } from "@/shared/repositories/query-params";
+import { Category } from "../entities/category-entity";
+
+export abstract class CategoryRepository {
+  abstract create(category: Category): Promise<Category>;
+  abstract findByName(name: string): Promise<Category | null>;
+  abstract findAll(queryParams?: IQueryParams): Promise<IPaginatedResult<Category>>;
+}
