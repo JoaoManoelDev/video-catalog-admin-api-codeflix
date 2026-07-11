e416f961f33487741623cd935353056a372f8505
João Manoel <joaomanoeldev@gmail.com>
2026-07-11 12:40:00 -0300

    feat: create PrismaCategoryMapper

---
+++ src/infra/database/prisma/mappers/prisma-category-mapper.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityID } from "@/shared/entities/unique-entity-id";
+import { Category } from "@/modules/category/domain/entities/category-entity";
+import { Category as PrismaCategory, Prisma } from "@/generated/prisma/client";
+
+export class PrismaCategoryMapper {
+  static toDomain(raw: PrismaCategory): Category {
+    return Category.create(
+      {
+        name: raw.name,
+        description: raw.description,
+        isActive: raw.isActive,
+        createdAt: raw.createdAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+
+  static toPrisma(category: Category): Prisma.CategoryUncheckedCreateInput {
+    const data = category.toJSON();
+
+    return {
+      id: data.id,
+      name: data.name,
+      description: data.description,
+      isActive: data.isActive,
+      createdAt: data.createdAt,
+    };
+  }
+}
