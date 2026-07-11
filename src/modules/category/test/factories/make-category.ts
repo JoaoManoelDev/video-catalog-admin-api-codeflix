a59cb44dc98a76fec4ac860cb586f119f7918c4b
João Manoel <joaomanoeldev@gmail.com>
2026-07-11 16:58:42 -0300

    feat: create CategoryFactory

--- src/modules/category/test/factories/make-category.ts
+++ src/modules/category/test/factories/make-category.ts
@@ -1,8 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { faker } from "@faker-js/faker";
+import { randomUUID } from "node:crypto";
+
 import { UniqueEntityID } from "@/shared/entities/unique-entity-id";
 import {
   Category,
   ICategoryProps,
 } from "@/modules/category/domain/entities/category-entity";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaCategoryMapper } from "@/infra/database/prisma/mappers/prisma-category-mapper";
 
 export const makeCategory = (
   override: Partial<ICategoryProps> = {},
@@ -20,3 +26,24 @@ export const makeCategory = (
 
   return category;
 };
+
+@Injectable()
+export class CategoryFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaCategory(
+    data: Partial<ICategoryProps> = {},
+  ): Promise<Category> {
+    const category = makeCategory({
+      name: faker.commerce.department() + " " + randomUUID().slice(0, 8),
+      description: faker.lorem.sentence(),
+      ...data,
+    });
+
+    await this.prisma.category.create({
+      data: PrismaCategoryMapper.toPrisma(category),
+    });
+
+    return category;
+  }
+}
