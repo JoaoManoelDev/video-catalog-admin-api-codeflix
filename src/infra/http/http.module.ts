3fd7d74779b88b15a0adb42b883e305871337ca1
João Manoel <joaomanoeldev@gmail.com>
2026-07-12 21:17:39 -0300

    chore: add controllers and providers for HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,15 +1,33 @@
 import { Module } from "@nestjs/common";
 
 import { CreateCategoryUseCase } from "@/modules/category/application/use-cases/create-category";
+import { DeleteCategoryUseCase } from "@/modules/category/application/use-cases/delete-category";
 import { FetchCategoriesUseCase } from "@/modules/category/application/use-cases/fetch-categories";
+import { GetCategoryByIdUseCase } from "@/modules/category/application/use-cases/get-category-by-id";
+import { UpdateCategoryUseCase } from "@/modules/category/application/use-cases/update-category";
 import { DatabaseModule } from "@/infra/database/database.module";
 
 import { CreateCategoryController } from "./controllers/create-category.controller";
+import { DeleteCategoryController } from "./controllers/delete-category.controller";
 import { FetchCategoriesController } from "./controllers/fetch-categories.controller";
+import { GetCategoryByIdController } from "./controllers/get-category-by-id.controller";
+import { UpdateCategoryController } from "./controllers/update-category.controller";
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateCategoryController, FetchCategoriesController],
-  providers: [CreateCategoryUseCase, FetchCategoriesUseCase],
+  controllers: [
+    CreateCategoryController,
+    FetchCategoriesController,
+    GetCategoryByIdController,
+    UpdateCategoryController,
+    DeleteCategoryController,
+  ],
+  providers: [
+    CreateCategoryUseCase,
+    FetchCategoriesUseCase,
+    GetCategoryByIdUseCase,
+    UpdateCategoryUseCase,
+    DeleteCategoryUseCase,
+  ],
 })
 export class HttpModule {}
