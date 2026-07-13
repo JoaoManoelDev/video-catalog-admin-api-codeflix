efb55a64438861dea23f077a80fb50907511a1ee
João Manoel <joaomanoeldev@gmail.com>
2026-07-12 21:14:26 -0300

    feat: create findById, save and delete methods

--- src/modules/category/domain/repositories/category-repository.ts
+++ src/modules/category/domain/repositories/category-repository.ts
@@ -4,6 +4,9 @@ import { Category } from "../entities/category";
 
 export abstract class CategoryRepository {
   abstract create(category: Category): Promise<Category>;
+  abstract findById(id: string): Promise<Category | null>;
   abstract findByName(name: string): Promise<Category | null>;
   abstract findAll(queryParams?: IQueryParams): Promise<IPaginatedResult<Category>>;
+  abstract save(category: Category): Promise<void>;
+  abstract delete(category: Category): Promise<void>;
 }
